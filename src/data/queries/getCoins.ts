50228000d63a9c506bc46dae6914b099120111bc
harrison rogers <harrison.rogers05@gmail.com>
2025-10-27 22:21:07 +1300

    refactor: implement new server fetch

--- src/data/queries/getCoins.ts
+++ src/data/queries/getCoins.ts
@@ -5,26 +5,11 @@ import type { CoinsResponse } from '../types';
  * @returns Promise that resolves to an array of coin data
  */
 const fetchCoins = async (): Promise<CoinsResponse> => {
-  const apiKey = import.meta.env.VITE_COIN_GECKO_API_KEY;
-
-  // Validate that the API key exists
-  if (!apiKey) {
-    throw new Error('VITE_COIN_GECKO_API_KEY environment variable is not set');
-  }
-
-  const fetchUrl =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&price_change_percentage=1h%2C24h%2C7d';
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      'x-cg-demo-api-key': apiKey,
-    },
-  };
+  const fetchUrl = import.meta.env.VITE_COINS_API;
 
   try {
     // Make the API request
-    const response = await fetch(fetchUrl, options);
+    const response = await fetch(fetchUrl);
 
     // Check if the request was successful
     if (!response.ok) {
