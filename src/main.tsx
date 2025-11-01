7933d8928fcd0a54c28210eb99ad11691f144a1c
harrison rogers <harrison.rogers05@gmail.com>
2025-11-02 12:00:28 +1300

    feat: add speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import App from './App.tsx';
 import Providers from './providers/providers.tsx';
 import Footer from './components/footer.tsx';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Providers>
-      <Analytics />
       <div className="max-w-[1400px] mx-auto mt-5 md:mt-10 h-[calc(100vh-40px)] flex flex-col justify-between">
         <App />
         <Footer />
+
+        {/* Analytics and Speed Insights */}
+        <Analytics />
+        <SpeedInsights />
       </div>
     </Providers>
   </StrictMode>
