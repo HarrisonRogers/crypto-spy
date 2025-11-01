69f3312b8feebdb4518ae8afe15a081b74d3c061
harrison rogers <harrison.rogers05@gmail.com>
2025-11-01 20:57:47 +1300

    feat: add the tabs and input

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,6 +3,7 @@ import { useChromeExtension } from '@/hooks/useChromeExtension';
 import { Button } from '@/components/ui/button';
 import { ArrowDownToLine } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 function App() {
   const isExtension = useChromeExtension();
@@ -29,7 +30,22 @@ function App() {
             </Button>
           )}
         </div>
-        <CoinTable />
+        <Tabs defaultValue="coins">
+          <TabsList className="self-center max-w-[600px] w-full mb-4">
+            <TabsTrigger value="coins" className="cursor-pointer">
+              Coins
+            </TabsTrigger>
+            <TabsTrigger value="dex" className="cursor-pointer">
+              Dex
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="coins">
+            <CoinTable />
+          </TabsContent>
+          <TabsContent value="dex">
+            <h1>Dex</h1>
+          </TabsContent>
+        </Tabs>
       </div>
     </>
   );
