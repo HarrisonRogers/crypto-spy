8900e67e4c4190314d1e90bf405a65e448b86469
harrison rogers <harrison.rogers05@gmail.com>
2025-11-02 11:55:08 +1300

    chore: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import Providers from './providers/providers.tsx';
 import Footer from './components/footer.tsx';
+import { Analytics } from '@vercel/analytics/next';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Providers>
+      <Analytics />
       <div className="max-w-[1400px] mx-auto mt-5 md:mt-10 h-[calc(100vh-40px)] flex flex-col justify-between">
         <App />
         <Footer />
